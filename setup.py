75d30af7bb74e1978cb11009bfd0fa5806d427f1
bw.song <13277096902@163.com>
2024-12-05 09:44:43 +0800

    切换默认分支

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="TerminatorCore",  # 包名
-    version="0.1.0",    # 包的版本
-    packages=find_packages(where='TerminatorBaseCore'),  # 自动发现包内的所有模块
+    version="0.1.1",    # 包的版本
+    packages=find_packages(),  # 自动发现包内的所有模块
     install_requires=[   # 依赖项
         "annotated-types==0.7.0",
         "asgiref==3.8.1",
@@ -26,7 +26,7 @@ setup(
     author="bw.song",  # 作者
     author_email="m13277096902@gmail.com",  # 作者邮箱
     description="django增强",  # 包的简短描述
-    long_description=open('README.md').read(),  # 从 README 文件读取更详细的描述
+    long_description=open('README.md', encoding='utf-8').read(),  # 从 README 文件读取更详细的描述
     long_description_content_type="text/markdown",  # README 文件格式
     classifiers=[  # 项目分类，用于 PyPI
         "Programming Language :: Python :: 3",
@@ -34,4 +34,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',  # 兼容的 Python 版本
+    license="MIT",
+    keywords="Terminator,SONG"
 )
